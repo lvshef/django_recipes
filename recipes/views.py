dbf9bd7bba829ccb04452d896ce3cd80076a30b7
lvshef <vitormdluzz2003@gmail.com>
2022-07-31 15:48:17 -0300

    first_commit

---
+++ recipes/views.py
@@ -0,0 +1,39 @@
+from django.http import Http404
+from django.shortcuts import get_list_or_404, render, get_object_or_404
+from utils.recipes.factory import make_recipe
+from .models import Category, Recipe
+
+
+def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
+    })
+
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name}'
+    })
+
+
+def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True
+    ).order_by('-id').first()
+
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
